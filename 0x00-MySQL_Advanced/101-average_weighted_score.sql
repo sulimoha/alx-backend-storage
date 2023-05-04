6c9d471899362f4bfaea823bb86d295a3e93019d
Mohamed Suliman <sulimmoh@gmail.com>
2023-05-04 17:16:24 -0400

    Average weighted score for all!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,25 +1,30 @@
--- Creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
-DELIMITER $$
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers
+AS
 BEGIN
-    DECLARE average_weighted FLOAT;
-    DECLARE i INT DEFAULT 0;
+    -- create a temporary table to store weighted scores for all students
+    CREATE TABLE #temp (
+        user_id INT,
+        weighted_score FLOAT
+    )
 
-    WHILE i < (SELECT COUNT(*) FROM users) DO
-        SET @user_id = (SELECT id FROM users ORDER BY id ASC LIMIT 1 OFFSET i);
-        SET @score = (SELECT score FROM corrections
-            WHERE user_id = @user_id LIMIT 1 OFFSET i);
-        SET @weight = (SELECT weight FROM projects
-            JOIN corrections ON corrections.project_id = projects.id
-            WHERE user_id = @user_id LIMIT 1 OFFSET i);
+    -- calculate and store the weighted score for each student
+    INSERT INTO #temp
+    SELECT u.user_id, SUM(e.score * c.weight) / SUM(c.weight)
+    FROM users u
+    JOIN enrollments en ON u.user_id = en.user_id
+    JOIN courses c ON en.course_id = c.course_id
+    JOIN exams e ON en.enrollment_id = e.enrollment_id
+    GROUP BY u.user_id
 
-        IF @score IS NOT NULL AND @weight IS NOT NULL THEN
-            SET average_weighted = SUM(@score * @weight) / SUM(@weight);
-            UPDATE users SET average_score = average_weighted WHERE id = @user_id;
-        END IF;
+    -- calculate the average weighted score for all students and store it in a variable
+    DECLARE @avg_weighted_score FLOAT
+    SELECT @avg_weighted_score = AVG(weighted_score)
+    FROM #temp
 
-        SET i = i + 1;
-    END WHILE;
-END$$
-DELIMITER ;
+    -- update the users table with the average weighted score for each user
+    UPDATE users
+    SET avg_weighted_score = @avg_weighted_score
+
+    -- drop the temporary table
+    DROP TABLE #temp
+END
