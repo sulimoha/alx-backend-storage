329184bdc6e7968d287d9c488330085a3f091ccf
Mohamed Suliman <sulimmoh@gmail.com>
2023-05-04 17:22:08 -0400

    Average weighted score for all!

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,3 +1,4 @@
+-- Creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
 DELIMITER $$
 DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
@@ -8,18 +9,14 @@ BEGIN
     WHILE i < (SELECT COUNT(*) FROM users) DO
         SET @user_id = (SELECT id FROM users ORDER BY id ASC LIMIT 1 OFFSET i);
         SET @score = (SELECT score FROM corrections
-            WHERE user_id = @user_id LIMIT 1);
+            WHERE user_id = @user_id LIMIT 1 OFFSET i);
         SET @weight = (SELECT weight FROM projects
             JOIN corrections ON corrections.project_id = projects.id
-            WHERE user_id = @user_id LIMIT 1);
+            WHERE user_id = @user_id LIMIT 1 OFFSET i);
 
         IF @score IS NOT NULL AND @weight IS NOT NULL THEN
-            SET average_weighted = (SELECT SUM(score * weight) FROM corrections
-                JOIN projects ON corrections.project_id = projects.id
-                WHERE corrections.user_id = @user_id) / (SELECT SUM(weight) FROM projects
-                JOIN corrections ON corrections.project_id = projects.id
-                WHERE corrections.user_id = @user_id);
-            UPDATE users SET average_weighted_score = average_weighted WHERE id = @user_id;
+            SET average_weighted = SUM(@score * @weight) / SUM(@weight);
+            UPDATE users SET average_score = average_weighted WHERE id = @user_id;
         END IF;
 
         SET i = i + 1;
