0322f8b9b02076c68190bba87a78fe51290c5684
Mohamed Suliman <sulimmoh@gmail.com>
2023-05-10 17:05:22 -0400

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,10 +11,10 @@ def count_calls(method: Callable) -> Callable:
     key = method.__qualname__
 
     @wraps(method)
-    def wrapper(self, *args, **kwargs) -> Any:
+    def wrapper(self, *args) -> Any:
         """ Wrapper method """
         self._redis.incr(key)
-        return method(self, *args, **kwargs)
+        return method(self, *args)
 
     return wrapper
 
@@ -24,17 +24,30 @@ def call_history(method: Callable) -> Callable:
     key = method.__qualname__
 
     @wraps(method)
-    def wrapper(self, *args, **kwargs) -> Any:
+    def wrapper(self, *args) -> Any:
         """ Wrapper method """
-        input = str(args)
-        self._redis.rpush(key + ':inputs', input)
+        inputs = str(args)
+        self._redis.rpush(f'{key}:inputs', inputs)
 
-        output = str(method(self, *args, **kwargs))
-        self._redis.rpush(key + ':outputs', output)
+        outputs = str(method(self, *args))
+        self._redis.rpush(f'{key}:outputs', outputs)
 
-        return output
+        return outputs
 
     return wrapper
+
+
+def replay(method: Callable) -> None:
+    """ Display the history of calls """
+    key = method.__qualname__
+    self = method.__self__
+    count = self._redis.get(key).decode('utf-8')
+    print(f'{key} was called {count} times:')
+
+    inputs = self._redis.lrange(f'{key}:inputs', 0, -1)
+    outputs = self._redis.lrange(f'{key}:outputs', 0, -1)
+    for i, o in zip(inputs, outputs):
+        print(f"{key}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
 
 
 class Cache:
@@ -55,7 +68,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float, None]:
         """ Get data and convert it the desired format """
         data = self._redis.get(key)
         if not data:
